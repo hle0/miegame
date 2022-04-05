678692467dff93ed172a5994194d9524d8185398
hle0 <91701075+hle0@users.noreply.github.com>
2022-04-04 20:50:38 -0400

    add test canvas to window

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::time::Duration;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -11,9 +12,16 @@ fn main() {
         .build()
         .unwrap();
 
+    let mut canvas = window.into_canvas().build().unwrap();
     let mut event_pump = sdl_context.event_pump().unwrap();
 
+    canvas.set_draw_color(Color::BLUE);
+    canvas.clear();
+
     'running: loop {
+        canvas.clear();
+        canvas.present();
+
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
