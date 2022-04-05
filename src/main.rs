dd2af7e9383ee372dca8cec653d7ff827223539b
hle0 <91701075+hle0@users.noreply.github.com>
2022-04-04 20:33:26 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::time::Duration;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
+fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("test", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+
+        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 120));
+    }
+
+    println!("Hello, world!");
+}
